0ebef6d30f4f9f398cfb8586388d6342d00b10c8
Esparzarr <61842004+Esparzarr@users.noreply.github.com>
2020-09-09 17:04:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 //Funciones en JS
 
-const 
+// const saludar = function (nombre){
+//     return `Hola, ${nombre}`;
+// }
+
+const saludar2 = (nombre) => {
+    return `Hola, ${nombre}`; 
+}
+
+const saludar3 = (nombre) => `Hola, ${nombre}`;
+const saludar4 = () => `Hola Mundo`;
+
+console.log(saludar2('Vegueta'));
+console.log(saludar3('Goku'));
+console.log(saludar4());
