188b894ab2f6898706ecdf070555d967e64ab473
Esparzarr <61842004+Esparzarr@users.noreply.github.com>
2020-09-11 17:12:59 -0500

    News

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({   value }) => {
+    return (
+        <>
+            <h1>CounterApp </h1>
+            <p>{ value }</p>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    CounterApp: PropTypes.string,
+    value: PropTypes.number
+}
+
+
+export default CounterApp;
