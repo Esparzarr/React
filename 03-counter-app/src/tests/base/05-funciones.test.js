1191593668fdd0bba632b99848035cb250b3b718
Esparzarr <61842004+Esparzarr@users.noreply.github.com>
2020-09-14 17:02:37 -0500

    News

---
+++ 03-counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,17 @@
+import { getUser } from '../../base/05-funciones';
+import '@testing-library/jest-dom';
+
+describe(' Pruebas en 05-funciones', () => {
+    test(' getUser debe retornar un objeto ', () => {
+        
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+        
+        expect( user ).toBe( userTest );
+    })
+    
+})
